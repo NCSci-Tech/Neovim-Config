fc17002c34f4b2a06390fd3302e8d4385039e32c
Nikolai <nicholasconnelly.sci@gmail.com>
2026-04-13 14:53:01 -0400

    Configure basic Vim settings and diagnostics
    Added basic settings for line numbers and colors.

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
+-- leader key
 vim.g.mapleader = " "
 
--- basic settings
+-- basic settings (optional but nice)
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 
+-- diagnostics module
+require("config.diagnostics").setup()
+
+-- load lazy
 require("config.lazy")
