ba26d0f0290efe73cce1482d1fe5ec32babcb500
Nikolai <nicholasconnelly.sci@gmail.com>
2026-04-15 13:04:49 -0400

    Enhance init.lua with basic settings and tabs
    Added basic settings and tab configuration options.

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
 -- leader key
 vim.g.mapleader = " "
 
--- basic settings (optional but nice)
+-- basic settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+
+-- Tab stuff
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
 -- diagnostics module
 require("config.diagnostics").setup()
